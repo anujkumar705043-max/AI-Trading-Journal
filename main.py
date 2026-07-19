14dea2f714cc831dc3e82fb2922874cc8cc75083
anujkumar705043-max <anujkumar705043@gmail.com>
2026-07-19 13:24:33 +0530

    Update CORS for production

--- main.py
+++ main.py
@@ -65,9 +65,10 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:3000",
-        "http://localhost:5173"
-    ],
+    "http://localhost:3000",
+    "http://localhost:5173",
+    "https://ai-trading-journal-seven.vercel.app"
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
